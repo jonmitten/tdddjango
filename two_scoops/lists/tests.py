63d7ab02b2f721fbfad3776bb9f425f5c0933db7
jonmitten <jonmitten@gmail.com>
2017-03-22 15:26:35 -0700

    ignores pyc

--- two_scoops/lists/tests.py
+++ two_scoops/lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Can we resolve the URL for the root of the site to a
 # particular view function we've made?
@@ -12,3 +13,10 @@ class HomePageTest(TestCase):
 
 # Can we make this view function return some HTML which will get the 
 # functional test to pass?
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do List</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))

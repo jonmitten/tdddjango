e14e5a9a22dd397316fe479ff792978ebedb2c2e
jonmitten <jonmitten@gmail.com>
2017-03-22 15:12:54 -0700

    first passing unit test

--- two_scoops/lists/tests.py
+++ two_scoops/lists/tests.py
@@ -1,7 +1,14 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
-class SmoteTest(TestCase):
+# Can we resolve the URL for the root of the site to a
+# particular view function we've made?
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+# Can we make this view function return some HTML which will get the 
+# functional test to pass?
